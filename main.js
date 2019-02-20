91824825a0a2415d9d2b75c5e89e47d3c227e619
Cian O'Ruanaidh <cianor.social@gmail.com>
2019-02-19 22:50:41 -0500

    potential pseudo code update - Cian

--- main.js
+++ main.js
@@ -3,3 +3,24 @@
 //      - For Namespacing
 //2. 
 //
+
+
+// Pseudo code idea - Cian
+    // Pseudo code for MVP
+
+    // declare app object
+
+    // declare initial object variables
+    // api keys
+    // api urls
+
+    // document ready function with init() function called within
+
+    // init function declaration
+        // event listener on button to call random recipe
+        // listener calls recipe function
+
+    // recipe function
+        // ajax call to query Yummly api
+        // randomly select reciper
+        // chain .then() function to show response response on our site
