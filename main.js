32f56d9f21c8d0663b510f88835af967ffee7082
Jennifer <jenn_e_fur16@hotmail.com>
2019-02-20 18:41:58 -0500

    first query and 3 random recipes

--- main.js
+++ main.js
@@ -6,7 +6,8 @@
     const app = {};
     app.apiKey = 'c29c1aea1bc9d6b87f4f2175fcbf13c4';
     app.apiId = '8423c766';
-    app.apiurl = 'http://api.yummly.com/v1/api/recipes';
+    app.apiUrl = 'http://api.yummly.com/v1/api/recipes';
+    let query = "chicken"
      
     // document ready function with init() function called within
     $(function(){
@@ -14,14 +15,37 @@
     });
 
     app.init = function(){
-        console.log('this is init');
+       // declare initial object variables
+        app.getRecipe(query);
     };
-    // declare initial object variables
-    // api keys
-    // api urls
 
-   
+    app.getRecipe = function(query){
+        $.ajax({
+            // api urls
+            url: app.apiUrl,
+            method:'GET',
+            dataType:'json',
+            data: {
+             // api keys
+                _app_key: app.apiKey,
+                _app_id: app.apiId,
+                format:'json',
+                q: query,
+                maxResult: 100,
+            },
 
+        }).then(function(results){
+            let responseArray = results.matches;
+            let recipeArray = [];
+            for(i = 0; i < 3; i++){
+                let index = Math.floor(Math.random() * 100);
+                recipeArray.push(responseArray[index]);
+                
+            }
+            console.log(recipeArray);     
+        })
+    }
+    
     // init function declaration
         // event listener on button to call random recipe
         // listener calls recipe function
