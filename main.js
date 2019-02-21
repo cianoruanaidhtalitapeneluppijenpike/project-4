6b3e0f28eef3e0b22221d9e904579fd6ad28dbed
Jennifer <jenn_e_fur16@hotmail.com>
2019-02-21 17:56:55 -0500

    more js 💪

--- main.js
+++ main.js
@@ -7,16 +7,17 @@
     app.apiKey = 'c29c1aea1bc9d6b87f4f2175fcbf13c4';
     app.apiId = '8423c766';
     app.apiUrl = 'http://api.yummly.com/v1/api/recipes';
-     
+    app.recipesArray = [];
     // document ready function with init() function called within
     $(function(){
-        app.init();
+        app.init();  
     });
 
     // init function declaration
     app.init = function(){
        // declare initial object variables
         $('.user-input').on('submit', function(e){
+            // event listener on button to call random recipe
             e.preventDefault();
             query = $('#search-recipe').val();
             queryType = typeof(query);
@@ -28,8 +29,8 @@
                 $('#search-recipe').val('');           
             }
         });
+        
         // create event listener on "show recipes" button to call .getRecipte() function
-
     };
 
     // recipe function
@@ -58,7 +59,6 @@
                 // Add eror handeling message - no results
                 console.log("THERE ARE NO RESULTS");
             } else {
-                console.log(results);
                 let responseArray = results.matches;
                 let recipeArray = [];
                 for(i = 0; i < 3; i++){
@@ -72,31 +72,48 @@
     
     // print results in the dom
     app.displayRecipes = function(array){
+        app.recipesArray = [];
+        let i = 0;
         array.forEach(function(item) {
             const title = item.recipeName;
             const image = item.imageUrlsBySize;
-            console.log(image);
             // api returns 90px x 90px image
             // manipulate url to return a larger (700px x 700px) image than supplied by api
             const bigImage = image[90].slice(0, image[90].length -4) + "700-c";
             const ingredients = item.ingredients;
             const time = item.totalTimeInSeconds;
-
+            const recipeid =  item.id;
             $('.gallery')
-                .append(`<div class="photo-box"><h2>${title}</h2><img src="${bigImage}" alt="${title}"></div>`);
-        });
-    
-    
-        
-    }
-    
-    
-        
-    
-    // event listener on button to call random recipe
-        // listener calls recipe function
+                .append(`<a id="${i}" href="#recipe-clicked"><div class="photo-box"><h2>${title}</h2><img src="${bigImage}" alt="${title}"></div></a>`);
 
-    
+            const recipe = {
+                title:title,
+                image:bigImage,
+                ingredients:ingredients,
+                time:time,
+                recipeid:recipeid,
+            };
+            app.recipesArray.push(recipe);
+            i++;
+        }); 
+        app.displaySelected(this); 
+    }
+
+    app.selectedRecipe = {}; 
+    app.displaySelected = function(selectedRecipe){
+        $('a').on('click', function () {
+            const i = $(this).attr(`id`);
+            console.log($(this).attr(`id`));
+            $('.recipe-title').append(app.recipesArray[i].title);
+            $('.selected-image').attr('src', app.recipesArray[i].image);
+            app.recipesArray[i].ingredients.forEach(function(item){
+                $('.ingredient-list').append(`<li>${item}</li>`);
+            });
+           
+            $('.cooktime').append(app.recipesArray[i].time / 60 + " minutes");
+            // console.log(app.recipesArray[i].recipeid);
+        });
+    };
 
 
 // STRETCH GOALS
